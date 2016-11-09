9e33bde3a467fe3757c18b164526118170da2752
pioner <starmuratovo@gmail.com>
2016-11-09 08:14:35 +0300

    Finish testing models Question & Answer

---
+++ db/migrate/20161109050914_add_index_question_id_to_answers.rb
@@ -0,0 +1,5 @@
+class AddIndexQuestionIdToAnswers < ActiveRecord::Migration[5.0]
+  def change
+    add_index :answers, :question_id
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,22 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161109045930) do
-
+ActiveRecord::Schema.define(version: 20_161_109_050_914) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  enable_extension 'plpgsql'
 
-  create_table "answers", force: :cascade do |t|
-    t.text     "body"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table 'answers', force: :cascade do |t|
+    t.text     'body'
+    t.datetime 'created_at',  null: false
+    t.datetime 'updated_at',  null: false
+    t.integer  'question_id'
+    t.index ['question_id'], name: 'index_answers_on_question_id', using: :btree
   end
 
-  create_table "questions", force: :cascade do |t|
-    t.string   "title"
-    t.text     "body"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table 'questions', force: :cascade do |t|
+    t.string   'title'
+    t.text     'body'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
   end
-
 end
