92be847e5f8df63d7e05ab868a941800f2037e72
pioner <starmuratovo@gmail.com>
2016-11-11 09:45:52 +0300

    Testing edit view

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,5 +1,5 @@
 class QuestionsController < ApplicationController
-  before_action :set_question, only: [:show]
+  before_action :set_question, only: [:show, :edit, :update]
 
   def index
     @questions = Question.all
@@ -12,10 +12,34 @@ class QuestionsController < ApplicationController
     @question = Question.new
   end
 
+  def edit
+  end
+=begin
+  def create
+    @question = Question.new(question_params)
+
+    if @question.save
+      redirect_to @question
+    else
+      render :new
+    end
+  end
+=end
+  def update
+    if @question.update_attributes(question_params)
+      redirect_to @question
+    else
+      render :edit
+    end
+  end
+
   private
 
     def set_question
       @question = Question.find(params[:id])
     end
 
+    def question_params
+      params.require(:question).permit(:title, :body)
+    end
 end
