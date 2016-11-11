207fc53fe17fac01b246758ef680567d65cea640
pioner <starmuratovo@gmail.com>
2016-11-11 18:49:01 +0300

    Finish testing Answer controller

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,3 +1,21 @@
 class AnswersController < ApplicationController
 
+  def create
+    @question = Question.find(params[:question_id])
+    @answer = @question.answers.build(answer_params)
+
+    if @answer.save
+      redirect_to @question
+      flash[:success] = "Congratulations! Answer created!"
+    else
+      render :new
+      flash[:error] = "Answer is not created! Try later!"
+    end
+  end
+
+  private
+
+    def answer_params
+      params.require(:answer).permit(:body,:question_id)
+    end
 end
