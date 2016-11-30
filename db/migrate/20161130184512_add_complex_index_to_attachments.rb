97f2070bd545ad74db31f7bcfd14c15e366330e4
pioner <starmuratovo@gmail.com>
2016-11-30 21:51:34 +0300

    Add complex index to attachments

---
+++ db/migrate/20161130184512_add_complex_index_to_attachments.rb
@@ -0,0 +1,8 @@
+class AddComplexIndexToAttachments < ActiveRecord::Migration[5.0]
+  def change
+    remove_index :attachments, :attachable_id
+    remove_index :attachments, :attachable_type
+
+    add_index :attachments, [:attachable_id, :attachable_type]
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161129122651) do
+ActiveRecord::Schema.define(version: 20161130184512) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,8 +32,7 @@ ActiveRecord::Schema.define(version: 20161129122651) do
     t.datetime "updated_at",      null: false
     t.integer  "attachable_id"
     t.string   "attachable_type"
-    t.index ["attachable_id"], name: "index_attachments_on_attachable_id", using: :btree
-    t.index ["attachable_type"], name: "index_attachments_on_attachable_type", using: :btree
+    t.index ["attachable_id", "attachable_type"], name: "index_attachments_on_attachable_id_and_attachable_type", using: :btree
   end
 
   create_table "questions", force: :cascade do |t|
